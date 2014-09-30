9ee80a96771385688eb92b8287d22094a1492775
Tobias Moebert <moebert@uni-potsdam.de>
2014-09-30 17:27:31 +0200

    # started implementing the logic for the adaptation and rule engine

--- scripts/motivate_adaptation_engine/adaptationengine.js
+++ scripts/motivate_adaptation_engine/adaptationengine.js
@@ -1,17 +1,72 @@
-var Class = easejs.Class;
+define("ae", ['re'], function (RuleEngine) {
+    var Class = easejs.Class;
 
-var AdaptationEngine = Class( 'AdaptationEngine',
-    {
-        'private ruleEngine': [],
-        'private contextDetector': [],
-        'private noolsDSL': "",
-
-        __construct: function(noolsDSL)
+    var AdaptationEngine = Class( 'AdaptationEngine',
         {
-            this.noolsDSL = noolsDSL;
+            'private _ruleEngine': [],
+            'private _contextDetector': [],
+            'private _noolsDSL': "",
+            'private _ruleMatchingInterval': null,
 
-            this.ruleEngine = new RuleEngine(noolsDSL);
+            'public isRunning': false,
+
+            __construct: function(noolsDSL)
+            {
+                this._noolsDSL = noolsDSL;
+                this._ruleEngine = new RuleEngine(noolsDSL);
+                // parse rules for context detection initialization
+                for (var rule in this._ruleEngine.getRules()) {
+                    // pass rule to context detection
+                }
+                FlowContextInformation = this._ruleEngine.getDefined("ContextInformation");
+                this._ruleEngine.addContextInformation(new FlowContextInformation("CurrentTemperatureMeasurableInformation", 55, {"TemperatureScaleContextParameter" : "FAHRENHEIT"}));
+            },
+
+            'public setRestrictFeatureCallback': function(callback) {
+                this._ruleEngine.setCallback("restrictFeatureCallback", callback);
+            },
+
+            'public setSelectLearningUnitCallback': function(callback) {
+                this._ruleEngine.setCallback("selectLearningUnitCallback", callback);
+            },
+
+            'public setPreloadLearningUnitCallback': function(callback) {
+                this._ruleEngine.setCallback("preloadLearningUnitCallback", callback);
+            },
+
+            'public setRuleMatchingSuccessCallback': function(callback) {
+                this._ruleEngine.setCallback("ruleMatchingSuccessCallback", callback);
+            },
+
+            'public setRuleMatchingErrorCallback': function(callback) {
+                this._ruleEngine.setCallback("ruleMatchingErrorCallback", callback);
+            },
+
+            'public startRuleMatching': function(intervalInMilliseconds) {
+                var that = this;
+
+                if (!this.isRunning) {
+                    this.isRunning = false;
+                    this._ruleEngine.matchRules();
+                    if (!isNaN(intervalInMilliseconds)) {
+                        this._ruleMatchingInterval = setInterval(function(){that._ruleEngine.matchRules()}, intervalInMilliseconds);
+                    }
+                }
+            },
+
+            'public stopRuleMatching': function() {
+                if (this.isRunning) {
+                    this.isRunning = false;
+                    clearInterval(this._ruleMatchingInterval);
+                }
+            },
+
+            'public restartRuleMatching': function(intervalInMilliseconds) {
+                this.stopRuleMatching();
+                this.startRuleMatching(intervalInMilliseconds);
+            }
         }
-    }
-);
+    );
 
+    return AdaptationEngine;
+});
--- scripts/motivate_adaptation_engine/ruleengine.js
+++ scripts/motivate_adaptation_engine/ruleengine.js
@@ -1,23 +1,94 @@
-var Class = easejs.Class;
+define("re", ['nools'], function (nools) {
+    var Class = easejs.Class;
 
-var RuleEngine = Class('RuleEngine',
-    {
-        'private nools': '',
-        'private flow': '',
-
-        __construct: function(noolsDSL)
+    var RuleEngine = Class('RuleEngine',
         {
-        	this.nools = require("nools");
-            this.flow = this.nools.parse(noolsDSL);
-            console.log(this.flow);
-			for (p in this.flow) {
-				if (p == 'rules') {
-					for (rule in p) {
-						console.log(rule);				
-					}
-				}
-			}
-        }
+            'private _nools': null,
+            'private _flow': null,
+            'private _parsedFlow': null,
+            'private _session': null,
+            'private _callbacks': {
+                "ruleMatchingSuccessCallback": function() {
+                    console.log("done");
+                },
+                "ruleMatchingErrorCallback": function(err) {
+                    console.error(err.stack);
+                }
+            },
 
-    }
-);
+            __construct: function(noolsDSL)
+            {
+                var that = this;
+
+                this._nools = require("nools");
+                this._parsedFlow = this._nools.parse(noolsDSL);
+                this._flow = this._nools.compile(noolsDSL, {name: "adaptationRules"});
+                this._session = this._flow.getSession();
+
+                this._session.on("restrictFeature", function(feature){
+                    console.log("Restrict Feature: "+feature+"!");
+                    if (typeof that._callbacks["restrictFeatureCallback"] != "undefined") {
+                        that._callbacks["restrictFeatureCallback"](feature);
+                    }
+                });
+
+                this._session.on("selectLearningUnit", function(id){
+                    console.log("Select Learning Unit: "+id+"!");
+                    if (typeof that._callbacks["selectLearningUnitCallback"] != "undefined") {
+                        that._callbacks["selectLearningUnitCallback"](id);
+                    }
+                });
+
+                this._session.on("preloadLearningUnit", function(id){
+                    console.log("Preload Learning Unit: "+id+"!");
+                    if (typeof that._callbacks["preloadLearningUnitCallback"] != "undefined") {
+                        that._callbacks["preloadLearningUnitCallback"](id);
+                    }
+                });
+            },
+
+            'public setCallback': function(callbackName, callback) {
+                this._callbacks[callbackName] = callback;
+            },
+
+            'public getRules': function() {
+                return this._parsedFlow.rules;
+            },
+
+            'public matchRules': function() {
+                var that = this;
+
+                console.log("matching...");
+                console.log(this._session.getFacts());
+                this._session.match(function(err) {
+                   if (err) {
+                       that._callbacks["ruleMatchingErrorCallback"](err);
+                   } else {
+                       that._callbacks["ruleMatchingSuccessCallback"]();
+                   }
+                });
+            },
+
+            'private _addFact': function(fact) {
+                this._session.assert(fact);
+            },
+
+            'public addContextInformation': function(contextInformation) {
+                //TODO: add context information directly from context detection and convert to flow context information
+                //FlowContextInformation = this.getDefined("ContextInformation");
+                //TODO: test if context information already exists
+                if (false) {
+
+                } else {
+                    this._addFact(contextInformation);
+                }
+            },
+
+            'public getDefined': function(definitionName) {
+                return this._flow.getDefined(definitionName);
+            }
+        }
+    );
+
+    return RuleEngine;
+});
